97cbee19a53946a2461950e9db13dbb6c7d7cc77
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-08-16 22:16:01 -0400

    added a result after game is done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,11 +9,11 @@ var lost = 0; //lost var
 //========================================================================
 // if the timer is over go to the next question
 function nextQuestion() {
+  var isQuestionOver = quizQuestions.length - 1 === currentQuestion;
 
-    var isQuestionOver = (quizQuestions.length -1) === currentQuestion;
-
-    if (isQuestionOver) {
-      console.log("Game is over");
+  if (isQuestionOver) {
+    console.log("Game is over");
+    displayResults();
   } else {
     currentQuestion++;
     loadQuestion();
@@ -56,13 +56,12 @@ function loadQuestion() {
     `);
 }
 
-
 // load question function
 function loadChoices(choices) {
   var result = "";
 
   for (var i = 0; i < choices.length; i++) {
-    result += `<button class="choice button-color" data-answer="${
+    result += `<button class=" btn btn-primary choice button-color" data-answer="${
       choices[i]
     }">${choices[i]}</button>`;
   }
@@ -73,57 +72,41 @@ function loadChoices(choices) {
 // Either wrong and right answer selected, go to the next question
 
 $(document).on("click", ".choice", function() {
-    clearInterval(timer);
-    var selectedAnswer = $(this).attr("data-answer");
-    var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
-// check if working
-    // console.log("yes", selectedAnswer);
+  clearInterval(timer);
+  var selectedAnswer = $(this).attr("data-answer");
+  var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+  // check if working
+  // console.log("yes", selectedAnswer);
 
-    if (correctAnswer === selectedAnswer){
-        console.log("yes");
-        score++
-        nextQuestion();
-    } else {
-        lost++;
-        console.log("no");
-        nextQuestion();
-    }
+  if (correctAnswer === selectedAnswer) {
+    console.log("yes");
+    score++;
+    nextQuestion();
+  } else {
+    lost++;
+    console.log("no");
+    nextQuestion();
+  }
 });
 
+// display the results
 
+function displayResults() {
+  var result = `
+<p> You get ${score} questions(s) right</p>
+<p> You missed ${lost} questions(s) right</p>
+<p> Total questions ${quizQuestions.length} questions(s) right</p>
+<button class="btn btn-primary button-color" id="reset"> Reset Game </button>
+    `;
+  $("#game").html(result);
+}
+
+function reset() {
+    $(document).on("click", "#reset", function(){
+
+    })
+}
 
 loadQuestion();
 
 //========================================================================
-
-// create a timer that when it ends it finishes the game and display win or lose
-
-// function run() {
-//   clearInterval(timer);
-//   // timer set to minus 1 sec
-//   timer = setInterval(decrement, 1000);
-// }
-
-// function decrement() {
-//   // decrease the number by 1
-//   counter--;
-
-//   // show the number in the #show-numer id tag
-//   $("#time").html("<h2> Time Left: " + counter + "</h2>");
-
-//   // testing
-//   console.log("timer");
-
-//   if (counter === 0) {
-//     stop();
-//     alert("Time's up!");
-//   }
-// }
-
-// // stop function
-// function stop() {
-//   clearInterval(timer);
-// }
-
-// //call run function
-// run();
